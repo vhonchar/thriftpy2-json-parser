2b4afb8204c66169d1f089e3505db869d06d0f02
Volodymyr Honchar <vhonchar94@gmail.com>
2023-04-03 15:39:52 -0700

    Refactored to reuse a single function to decompose all thrift definitions

--- src/json2thrift.py
+++ src/json2thrift.py
@@ -5,7 +5,6 @@ from typing import Any, cast
 
 from thriftpy2.thrift import TType
 
-
 class ThriftJSONDecoder(json.JSONDecoder):
 
     def __init__(self, *args, **kwargs):
@@ -41,15 +40,9 @@ class ThriftJSONDecoder(json.JSONDecoder):
         thrift_class = kwargs['thrift_class']
         thrift_spec = thrift_class.thrift_spec
         composable_obj = thrift_class()
+
         for field_def in thrift_spec.values():
-            if len(field_def) == 3:
-                (field_ttype, field_name, required) = field_def
-                field_class = None
-            elif len(field_def) == 4:
-                (field_ttype, field_name, field_class, required) = field_def
-            else:
-                raise ValueError(
-                    f"Received unknown length of a field definition {len(field_def)} in thrift spec {thrift_spec}")
+            field_ttype, field_name, field_class, required = self._decompose_thrift_def(field_def)
 
             if field_name not in value and required:
                 field_path_suffix = f"field {kwargs['field_path']}" if kwargs['field_path'] != '' else ''
@@ -76,27 +69,38 @@ class ThriftJSONDecoder(json.JSONDecoder):
         return bool(value)
 
     def _to_set(self, value: Any, **kwargs):
-        element_ttype, element_def = self._decompose_thrift_field_def(kwargs['thrift_class'])
+        element_ttype, _, element_def, _ = self._decompose_thrift_def(kwargs['thrift_class'])
         return {self._convert(x, element_ttype, kwargs['field_path'], element_def) for x in value}
 
     def _to_list(self, value: Any, **kwargs):
-        (element_ttype, element_def) = self._decompose_thrift_field_def(kwargs['thrift_class'])
+        element_ttype, _, element_def, _ = self._decompose_thrift_def(kwargs['thrift_class'])
         return [self._convert(x, element_ttype, kwargs['field_path'], element_def) for x in value]
 
     def _to_map(self, value: Any, **kwargs):
         thrift_class = kwargs['thrift_class']
-        (key_ttype, value_def) = thrift_class
-        (val_ttype, val_thrift_class) = self._decompose_thrift_field_def(value_def)
+        key_ttype, _, value_def, _ = self._decompose_thrift_def(thrift_class)
+        val_ttype, _, val_thrift_class, _ = self._decompose_thrift_def(value_def)
 
         return dict([(self._convert(k, key_ttype, kwargs['field_path']),
                       self._convert(v, val_ttype, kwargs['field_path'], val_thrift_class)) for (k, v) in value.items()])
 
-    def _decompose_thrift_field_def(self, field_def):
+    def _decompose_thrift_def(self, field_def) -> tuple[int, str | None, Any | None, bool | None]:
         if isinstance(field_def, int):
-            return field_def, None
+            field_ttype = field_def
+            field_name, field_class, required = None, None, None
+        elif len(field_def) == 2:
+            field_ttype, field_class = field_def
+            field_name, required = None, None
+        elif len(field_def) == 3:
+            field_ttype, field_name, required = field_def
+            field_class = None
+        elif len(field_def) == 4:
+            field_ttype, field_name, field_class, required = field_def
         else:
-            (field_ttype, field_class) = field_def
-            return field_ttype, field_class
+            raise ValueError(
+                f"Received unknown length of a field definition {len(field_def)} in thrift spec {field_def}")
+
+        return field_ttype, field_name, field_class, required
 
 
 def json2thrift(json_str: str | dict, thrift_class):
